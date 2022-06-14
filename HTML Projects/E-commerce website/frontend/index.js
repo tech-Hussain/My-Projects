6fd46326d92bf65ef0310da85aea08057268c905
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-14 14:01:14 +0500

    added

---
+++ HTML Projects/E-commerce website/frontend/index.js
@@ -0,0 +1,17 @@
+const changeui=()=>{
+    var x=0;
+    var entries=["Comfort","Quality","Reliability","Reasonable Price"]
+    setInterval(() => {
+        document.getElementById("blinking").classList.toggle("visibility")
+    }, 500);
+    // setInterval(()=>{
+    //     document.getElementById("changeui").innerHTML=entries[x]
+    //     x+=1;
+    //     if (x>3) {
+    //         x=0
+    //     }
+    // },1000)
+    setInterval(() => {
+        
+    }, 200);
+}
