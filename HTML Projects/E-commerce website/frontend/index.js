73c76d793fee204675c24fc12cdbcf37bf07bfb8
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-15 02:03:33 +0500

    added

--- HTML Projects/E-commerce website/frontend/index.js
+++ HTML Projects/E-commerce website/frontend/index.js
@@ -1,24 +1,76 @@
-var x=1;
-var entries=["Comfort","Quality","Reliability","Reasonable Price"]
+var x = 0;
+var y = 1;
+var entries = ["Comfort", "Quality", "Reliability", "Reasonable Price"];
 setInterval(() => {
-    document.getElementById("blinking").classList.toggle("visibility")
+  document.getElementById("blinking").classList.toggle("visibility");
 }, 500);
-const changeui=()=>{
-    interval=setInterval(() => {
-        var value=document.getElementById("change_content").innerHTML
-        document.getElementById("change_content").innerHTML=value.slice(0,value.length-1)
-        if (value.length==0){
-                document.getElementById("change_content").innerHTML=entries[x]
-                if (x==3) {
-                 x=0
-                } else {
-                  x+=1    
-                }  
-            clearInterval(interval)
-            setTimeout(() => {
-                changeui()
-                
-            }, 1000);
-        }
-    }, 100);
+function trim(str, length) {
+  document.getElementById("change_content").innerHTML = str.substr(0, length);
 }
+const decrease_func=()=>{
+const decrease = setInterval(() => {
+  var value = document.getElementById("change_content").innerHTML;
+  trim(value, value.length - 1);
+  if (value.length == 0) {
+    clearInterval(decrease);
+    if (x==3) {
+      x=0
+    }
+    else{
+    x += 1;
+    }
+  }
+}, 100);
+}
+const increase_func=()=>{
+const increase = setInterval(() => {
+  document.getElementById("change_content").innerHTML = entries[x];
+  var value_0 = document.getElementById("change_content").innerHTML;
+  trim(value_0, y);
+  value_0 = document.getElementById("change_content").innerHTML;
+  if (value_0 == entries[x]) {
+    clearInterval(increase);
+    y = 1;
+  }
+  else{
+    y+=1
+  }
+}, 100);
+}
+const changeui = () => {
+  setTimeout(() => {
+    setInterval(() => {
+      value_1=document.getElementById("change_content").innerHTML
+      if (value_1.length == 0) {
+        increase_func()
+      }
+      else if(value_1.length!=0){
+        decrease_func()
+      }
+    }, 2500);
+  }, 2500);
+};
+document.querySelector("#sale2").addEventListener("mouseenter",()=>{
+  document.querySelector("#sale2").classList.add("sale1")
+  document.querySelector("#sale2").classList.remove("sale2")
+  document.querySelector("#sale1").classList.add("sale2")
+  document.querySelector("#sale1").classList.remove("sale1")
+})
+document.querySelector("#sale2").addEventListener("mouseleave",()=>{
+  document.querySelector("#sale2").classList.add("sale2")
+  document.querySelector("#sale2").classList.remove("sale1")
+  document.querySelector("#sale1").classList.add("sale1")
+  document.querySelector("#sale1").classList.remove("sale2")
+})
+document.querySelector("#sale1").addEventListener("mouseenter",()=>{
+  document.querySelector("#sale2").classList.add("sale1")
+  document.querySelector("#sale2").classList.remove("sale2")
+  document.querySelector("#sale1").classList.add("sale2")
+  document.querySelector("#sale1").classList.remove("sale1")
+})
+document.querySelector("#sale1").addEventListener("mouseleave",()=>{
+  document.querySelector("#sale2").classList.add("sale2")
+  document.querySelector("#sale2").classList.remove("sale1")
+  document.querySelector("#sale1").classList.add("sale1")
+  document.querySelector("#sale1").classList.remove("sale2")
+})
