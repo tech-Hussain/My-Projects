7f9c3d117a7d141f5d051ee319d1ea0688bfc752
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-16 21:03:42 +0500

    added

--- HTML Projects/E-commerce website/frontend/index.js
+++ HTML Projects/E-commerce website/frontend/index.js
@@ -7,95 +7,121 @@ setInterval(() => {
 function trim(str, length) {
   document.getElementById("change_content").innerHTML = str.substr(0, length);
 }
-const decrease_func=()=>{
-const decrease = setInterval(() => {
-  var value = document.getElementById("change_content").innerHTML;
-  trim(value, value.length - 1);
-  if (value.length == 0) {
-    clearInterval(decrease);
-    if (x==3) {
-      x=0
+const decrease_func = () => {
+  const decrease = setInterval(() => {
+    var value = document.getElementById("change_content").innerHTML;
+    trim(value, value.length - 1);
+    if (value.length == 0) {
+      clearInterval(decrease);
+      if (x == 3) {
+        x = 0
+      }
+      else {
+        x += 1;
+      }
     }
-    else{
-    x += 1;
-    }
-  }
-}, 100);
+  }, 100);
 }
-const increase_func=()=>{
-const increase = setInterval(() => {
-  document.getElementById("change_content").innerHTML = entries[x];
-  var value_0 = document.getElementById("change_content").innerHTML;
-  trim(value_0, y);
-  value_0 = document.getElementById("change_content").innerHTML;
-  if (value_0 == entries[x]) {
-    clearInterval(increase);
-    y = 1;
-  }
-  else{
-    y+=1
-  }
-}, 100);
+const increase_func = () => {
+  const increase = setInterval(() => {
+    document.getElementById("change_content").innerHTML = entries[x];
+    var value_0 = document.getElementById("change_content").innerHTML;
+    trim(value_0, y);
+    value_0 = document.getElementById("change_content").innerHTML;
+    if (value_0 == entries[x]) {
+      clearInterval(increase);
+      y = 1;
+    }
+    else {
+      y += 1
+    }
+  }, 100);
 }
 const changeui = () => {
   setTimeout(() => {
     setInterval(() => {
-      value_1=document.getElementById("change_content").innerHTML
+      value_1 = document.getElementById("change_content").innerHTML
       if (value_1.length == 0) {
         increase_func()
       }
-      else if(value_1.length!=0){
+      else if (value_1.length != 0) {
         decrease_func()
       }
     }, 2500);
   }, 2500);
 };
-document.querySelector("#sale2").addEventListener("mouseenter",()=>{
+document.querySelector("#sale2").addEventListener("mouseenter", () => {
   document.querySelector("#sale2").classList.add("sale1")
   document.querySelector("#sale2").classList.remove("sale2")
   document.querySelector("#sale1").classList.add("sale2")
   document.querySelector("#sale1").classList.remove("sale1")
 })
-document.querySelector("#sale2").addEventListener("mouseleave",()=>{
+document.querySelector("#sale2").addEventListener("mouseleave", () => {
   document.querySelector("#sale2").classList.add("sale2")
   document.querySelector("#sale2").classList.remove("sale1")
   document.querySelector("#sale1").classList.add("sale1")
   document.querySelector("#sale1").classList.remove("sale2")
 })
-document.querySelector("#sale1").addEventListener("mouseenter",()=>{
+document.querySelector("#sale1").addEventListener("mouseenter", () => {
   document.querySelector("#sale2").classList.add("sale1")
   document.querySelector("#sale2").classList.remove("sale2")
   document.querySelector("#sale1").classList.add("sale2")
   document.querySelector("#sale1").classList.remove("sale1")
 })
-document.querySelector("#sale1").addEventListener("mouseleave",()=>{
+document.querySelector("#sale1").addEventListener("mouseleave", () => {
   document.querySelector("#sale2").classList.add("sale2")
   document.querySelector("#sale2").classList.remove("sale1")
   document.querySelector("#sale1").classList.add("sale1")
   document.querySelector("#sale1").classList.remove("sale2")
 })
-if (window.innerWidth<=870) {
-  let height=`${(document.querySelector(".menu").offsetTop)+(Number(window.getComputedStyle(document.querySelector(".menu"),null).getPropertyValue("height").slice(0,-2)))}px`
-  document.querySelector(".navbar").style.height=height
+if (window.innerWidth <= 870) {
+  let height = `${(document.querySelector(".menu").offsetTop) + (Number(window.getComputedStyle(document.querySelector(".menu"), null).getPropertyValue("height").slice(0, -2)))}px`
+  document.querySelector(".navbar").style.height = height
 }
-const menu=()=>{
-  let initialHeight=(document.querySelector(".menu").offsetTop)+(Number(window.getComputedStyle(document.querySelector(".menu"),null).getPropertyValue("height").slice(0,-2)))
-  let navHeight=window.getComputedStyle(document.querySelector(".navbar"),null).getPropertyValue("height").slice(0,-2)
-  let maxHeight=(document.querySelector(".nav").offsetTop)+(Number(window.getComputedStyle(document.querySelector(".nav"),null).getPropertyValue("height").slice(0,-2)))
+const menu = () => {
+  let initialHeight = (document.querySelector(".menu").offsetTop) + (Number(window.getComputedStyle(document.querySelector(".menu"), null).getPropertyValue("height").slice(0, -2)))
+  let navHeight = window.getComputedStyle(document.querySelector(".navbar"), null).getPropertyValue("height").slice(0, -2)
+  let maxHeight = (document.querySelector(".nav").offsetTop) + (Number(window.getComputedStyle(document.querySelector(".nav"), null).getPropertyValue("height").slice(0, -2)))
 
-  if ((Math.abs(navHeight-initialHeight))<(Math.abs(navHeight-maxHeight))) {
-    document.querySelector(".navbar").style.height=(maxHeight+50)+"px"
-    document.querySelector(".navbar").style.backdropFilter="blur(10px)"
+  if ((Math.abs(navHeight - initialHeight)) < (Math.abs(navHeight - maxHeight))) {
+    document.querySelector(".navbar").style.height = (maxHeight + 50) + "px"
+    document.querySelector(".navbar").style.backdropFilter = "blur(10px)"
   }
-  else if((Math.abs(navHeight-initialHeight))>(Math.abs(navHeight-maxHeight))){
-    document.querySelector(".navbar").style.height=initialHeight+"px"
+  else if ((Math.abs(navHeight - initialHeight)) > (Math.abs(navHeight - maxHeight))) {
+    document.querySelector(".navbar").style.height = initialHeight + "px"
     setTimeout(() => {
-      document.querySelector(".navbar").style.backdropFilter="blur(0px)"
-      
+      document.querySelector(".navbar").style.backdropFilter = "blur(0px)"
+
     }, 1000);
   }
 }
-if (window.innerWidth<=530) {
-  document.querySelector("#change_content").insertAdjacentHTML("beforebegin",`<br>`)
-  document.querySelector("#change_content").insertAdjacentHTML("beforebegin",`<br>`)
+if (window.innerWidth <= 530) {
+  document.querySelector("#change_content").insertAdjacentHTML("beforebegin", `<br>`)
+  document.querySelector("#change_content").insertAdjacentHTML("beforebegin", `<br>`)
 }
+                                         // SWIPER
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 40,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+
+
+
+
+
+
