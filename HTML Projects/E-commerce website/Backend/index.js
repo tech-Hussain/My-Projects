0c7ec41f629d99df1a1e48e0a2603883a84993c4
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-18 01:38:21 +0500

    added

--- HTML Projects/E-commerce website/Backend/index.js
+++ HTML Projects/E-commerce website/Backend/index.js
@@ -1,1 +1,7 @@
-console.log("welcome");
+const fs = require('fs');
+// fs.mkdirSync('thapa');
+// fs.writeFileSync("thapa/bio.txt","My name is Hussain")
+// fs.appendFileSync("thapa/bio.txt","My age is 17")
+// console.log(fs.readFileSync('thapa/bio.txt').toString())
+// fs.renameSync('thapa/bio.txt',"thapa/mybio.txt")
+fs.rmdirSync("thapa")
