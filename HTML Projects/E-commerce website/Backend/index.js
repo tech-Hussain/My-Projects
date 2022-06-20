2a87d929840b2edbe9f77df6dc06f0eb81b66d5d
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-20 13:37:31 +0500

    adde

--- HTML Projects/E-commerce website/Backend/index.js
+++ HTML Projects/E-commerce website/Backend/index.js
@@ -1,7 +1,2 @@
-const fs = require('fs');
-// fs.mkdirSync('thapa');
-// fs.writeFileSync("thapa/bio.txt","My name is Hussain")
-// fs.appendFileSync("thapa/bio.txt","My age is 17")
-// console.log(fs.readFileSync('thapa/bio.txt').toString())
-// fs.renameSync('thapa/bio.txt',"thapa/mybio.txt")
-fs.rmdirSync("thapa")
+import chalk from 'chalk';
+console.log(chalk.red("sucess"));
