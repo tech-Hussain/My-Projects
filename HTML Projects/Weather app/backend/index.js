a0afe74452b28af7db7f4b7b301b6add331fb535
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-23 16:54:03 +0500

    added

--- HTML Projects/Weather app/backend/index.js
+++ HTML Projects/Weather app/backend/index.js
@@ -1,6 +1,20 @@
-const fs = require('fs')
-const http = require('http')
-const data = fs.readFileSync('../frontend/index.html', 'utf-8')
+import fs from 'fs'
+import http from 'http'
+import fetch from 'node-fetch';
+const Apiresponse = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=3e4c955cdb10428fd983999f59f3b259');
+const Apidata = await Apiresponse.json();
+var data = fs.readFileSync('../frontend/index.html', 'utf-8')
+data=data.replace('{city}',Apidata.name)
+data=data.replace('{temp}',((Apidata.main.temp)-273.15).toPrecision(3))
+data=data.replace('{Realtemp}',((Apidata.main.feels_like)-273.15).toPrecision(3))
+data=data.replace('{weather}',Apidata.weather[0].main)
+data=data.replace('{mintemp}',((Apidata.main.temp_min)-273.15).toPrecision(3))
+data=data.replace('{maxtemp}',((Apidata.main.temp_max)-273.15).toPrecision(3))
+data=data.replace('{pressure}',Apidata.main.pressure)
+data=data.replace('{humidity}',Apidata.main.humidity)
+data=data.replace('{speed}',((Apidata.wind.speed)*3.6).toPrecision(3))
+data=data.replace('{visibility}',(Apidata.visibility)/1000)
+data=data.replace('{cover}',Apidata.clouds.all)
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
         res.write(data)
