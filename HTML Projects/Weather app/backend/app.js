0f35d5c02c73c5949d9011e90898f6ba8e3c12d8
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-28 00:50:15 +0500

    added

--- HTML Projects/Weather app/backend/app.js
+++ HTML Projects/Weather app/backend/app.js
@@ -45,6 +45,6 @@ app.get("/",async(req,res)=>{
 app.get("*",(req,res)=>{
     res.render("<h1></h1>")
 })
-app.listen(3000,'0.0.0.0',()=>{
+app.listen(port,()=>{
     console.log(`port listening on ${port}`);
 })
