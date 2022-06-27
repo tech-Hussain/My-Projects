4ec51a697324d282f0e619313bf3d88bde64394a
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-06-27 16:54:45 +0500

    added

--- HTML Projects/Weather app/backend/app.js
+++ HTML Projects/Weather app/backend/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import fetch from "node-fetch";
 import path from 'path';
 const port=process.env.PORT || 3000
-const staticPath=path.join(process.cwd(),'../frontend')
+const staticPath=path.join(process.cwd(),'../frontend/static')
+const dynamicPath=path.join(process.cwd(),'../frontend/dynamic')
 const app=express()
+app.set('views',dynamicPath)
+app.set('view engine','hbs')
 app.use(express.static(staticPath))
 app.get("/",(req,res)=>{
-    res.send("HI")
+    res.render('index')
 })
 app.listen(3000,()=>{
     console.log(`port listening on ${port}`);
