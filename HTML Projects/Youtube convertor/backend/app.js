3b7a3198120126a1af5a43b4a9b42c37a8fd533c
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-07-04 02:19:32 +0500

    added

--- HTML Projects/Youtube convertor/backend/app.js
+++ HTML Projects/Youtube convertor/backend/app.js
@@ -1,4 +1,29 @@
-import yt from 'yt-converter'
-yt.getInfo("https://www.youtube.com/watch?v=JJ5r5Z6G2Zo&list=RDGMEM2j3yRsqu_nuzRLnHd2bMVAVMJJ5r5Z6G2Zo&start_radio=1&ab_channel=SonyMusicIndiaVEVO").then(info => {
-    console.log(info.title);
-});
+import express from 'express'
+import path from 'path'
+import { Youtube } from 'ytdownloader.js'
+const staticPath=path.join(process.cwd(),'../frontend/static')
+const dynamicPath=path.join(process.cwd(),'../frontend/dynamic')
+const port=process.env.PORT || 5050
+const app=express()
+app.use(express.json())
+app.use(express.static(staticPath))
+app.use(express.urlencoded({
+    extended:true
+}))
+app.set("view engine","hbs")
+app.set("views",dynamicPath)
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+app.post("/",async(req,res)=>{
+    const data=await new Youtube().ytmp3(req.body.url, false)
+    console.log(data);
+    var link=data.dl_link
+    console.log(typeof(link));
+    res.render("index",{
+        link:data.dl_link
+    })
+})
+app.listen(port,()=>{
+    console.log(`listening to ${port}`);
+})
