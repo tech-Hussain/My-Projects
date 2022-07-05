221e54faf1e4cc2672f9e45772d1092b30d31648
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-07-06 01:17:34 +0500

    added

--- HTML Projects/E-commerce website/frontend/Static/signup.js
+++ HTML Projects/E-commerce website/frontend/Static/signup.js
@@ -30,8 +30,61 @@ if (window.innerWidth <= 870) {
     }
   }
 function validate() {
-    if (document.getElementsByName("username")[0].value.length<2) {
-      console.log("hi");
+  var errorkey={
+    "0%":{
+        visibility: "visible",
+        opacity: 0
+    },
+    "20%":{
+        opacity: 1,
+    },
+    "90%":{
+        opacity: 1,
+    },
+    "100%":{
+        visibility: "hidden",
+        opacity: 0,
+    }
+  }
+  var errorKey=[
+    {
+      visibility: "hidden",
+      opacity: 0
+    },
+    {
+      visibility: "visible",
+      opacity: 1
+    },
+    {
+      visibility: "visible",
+      opacity: 1
+    },
+    {
+      visibility: "visible",
+      opacity: 1
+    },
+    {
+      visibility: "visible",
+      opacity: 1
+    },
+    {
+      visibility: "hidden",
+      opacity: 0,
+    }
+  ]
+  var errorTime={
+    duration:5000,
+  }
+    if (document.getElementsByName("username")[0].value.length<3){
+      document.getElementById("uerror").animate(errorKey,errorTime)
+      return false;
+    }
+    if (document.getElementsByName("password")[0].value.length<6) {
+      document.getElementById("perror").animate(errorKey,errorTime)
+      return false;
+    }
+    if (document.getElementsByName("password")[0].value!=document.getElementsByName("cpassword")[0].value) {
+      document.getElementById("cperror").animate(errorKey,errorTime)
       return false;
     }
 }
