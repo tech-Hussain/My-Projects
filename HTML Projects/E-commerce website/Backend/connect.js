3afbe31b292b133800f4006b0f0b72bb20f2324a
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-07-08 12:49:17 +0500

    added

--- HTML Projects/E-commerce website/Backend/connect.js
+++ HTML Projects/E-commerce website/Backend/connect.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/Customers")
+const db="mongodb+srv://tech-Hussain:8979%4vW%23VeS%23qDf@cluster0.bgrkw.mongodb.net/Customers?retryWrites=true&w=majority"
+console.log(db);
+mongoose.connect(db,{
+    useNewUrlParser: true,
+     useUnifiedTopology: true,
+}
+)
 .then((data)=>{
     console.log("successfully connected to database");
 })
