b98a960176df793839d1e2fafd2a9cd99925fcd3
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-07-10 15:04:40 +0500

    added

--- HTML Projects/React Practice/my-app/src/index.js
+++ HTML Projects/React Practice/my-app/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+const date=new Date().getHours()
 
+var status,color;
+if (date>=1&&date<=11) {
+    status="Morning"
+    color="#ed9600"
+}
+else if (date>=12&&date<=19) {
+    status="Afternoon"
+    color="#edc600"
+}
+else{
+    status="Night"
+    color="#0a4170"
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render([
-<h1>Hello Hussain</h1>,
-<h2>Hussain</h2>
-]
+root.render(
+    <>
+        <span className='static'>Hello sir,</span><span style={{marginLeft:"1rem",color:color}}>Good {status}</span>
+    </>
 );
