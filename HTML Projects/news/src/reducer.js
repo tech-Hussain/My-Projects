981e3007f376f6deb84b5891726146c30d38010c
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-07-20 01:31:58 +0500

    added

--- HTML Projects/news/src/reducer.js
+++ HTML Projects/news/src/reducer.js
@@ -2,11 +2,24 @@
 const reducer = (state, action) => {
     switch (action.type) {
         case "Get_Data":
-            return({
-            hits: action.extras.hits,
-            page: action.extras.page,
-            nbPages: action.extras.nbPages,
-    })
+            return ({
+                ...state,
+                isLoading: false,
+                hits: action.extras.hits,
+                page: action.extras.page,
+                nbPages: action.extras.nbPages,
+                nbHits: action.extras.nbHits
+            })
+        case "change":
+            return ({
+                ...state,
+                query: action.extras,
+            })
+        case "delete":
+            return ({
+                ...state,
+                hits:action.extras
+            })
         default:
             break;
     }
