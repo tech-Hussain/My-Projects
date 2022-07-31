9da4a62bad0b2b41d9357bbddc1ea7233ff435a3
Syed Muhammad Hussain <smarif10569@gmail.com>
2022-08-01 01:47:52 +0500

    added

--- HTML Projects/reactapp/src/components/Contact.jsx
+++ HTML Projects/reactapp/src/components/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { PhoneIphone, LocationCity, Email } from "@mui/icons-material"
 import {ToastContainer,toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useFormik } from 'formik';
 const Contact = () => {
   const copy = (text) => {
     navigator.clipboard.writeText(text)
@@ -15,6 +16,18 @@ const Contact = () => {
       progress: undefined,
     })
   }
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phone: '',
+      msg: '',
+    },
+    onSubmit: (values,{resetForm}) => {
+      console.log(values)
+      resetForm({values:""})
+    },
+  });
   return (
     <div className='contactDiv'>
       <div className='myInfo'>
@@ -36,6 +49,47 @@ const Contact = () => {
           <span className='infoData' onClick={() => copy("Karachi, Sindh, Pakistan")}>Karachi, Sindh, Pakistan</span>
           <ToastContainer/>
         </div>
+      </div>
+      <div className='myMsg'>
+        <h3>Get in Touch</h3>
+        <form onSubmit={formik.handleSubmit} method="POST" className='myMsgDiv'>
+        <input
+         id="name"
+         name="name"
+         type="text"
+         spellCheck="false"
+         placeholder='Your Name'
+         onChange={formik.handleChange}
+         value={formik.values.name}
+       />
+       <input
+         id="email"
+         name="email"
+         type="email"
+         spellCheck="false"
+         placeholder='Your Email'
+         onChange={formik.handleChange}
+         value={formik.values.email}
+       />
+       <input
+         id="phone"
+         name="phone"
+         type="number"
+         spellCheck="false"
+         placeholder='Your Phone Number'
+         onChange={formik.handleChange}
+         value={formik.values.phone}
+       />
+       <textarea
+         id="msg"
+         name="msg"
+         spellCheck="false"
+         placeholder='Message'
+         onChange={formik.handleChange}
+         value={formik.values.msg}
+       />
+       <button type="submit">Send Message</button>
+        </form>
       </div>
     </div>
   )
